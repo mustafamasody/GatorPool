17f16286daf42fb18dd1a07d69a668537d9c616d
Hi-CZ <h.cruz1@ufl.edu>
2025-02-03 15:44:57 -0500

    add profile page

--- client/src/main/profile.tsx
+++ client/src/main/profile.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Button, Input } from '@heroui/react';
+import { Button, Input, Avatar, AvatarIcon} from '@heroui/react';
 import Navbar from './components/navbar';
 
 const ViewProfile = () => {
@@ -7,7 +7,15 @@ const ViewProfile = () => {
     return(
         <div className="light dark:dark bg-white dark:bg-black flex flex-col h-full min-h-full min-h-screen w-full">
         <Navbar />
-        
+            <div className="flex flex-col justify-center items-center mt-5">
+                <Avatar 
+                    src="/assets/images/cheshirecat.jpeg"
+                    onError={() => console.log('Error loading avatar image')}
+                >
+                    <AvatarIcon />
+                </Avatar>
+                
+            </div>
         </div>
     );
 };
